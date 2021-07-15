75fe1c89fa6f8d6f3f66aeac767d99d9a49b5bc3
Volodymyr <v.kalynii@gmail.com>
2021-07-16 00:20:42 +0300

    working with tesets

--- src/math-utils.ts
+++ src/math-utils.ts
@@ -23,28 +23,30 @@ export const getRandFromFifthPercents = () => random(0, 1);
  * tested for actions^ * / + -
  */
 export const calculateString = (str:string) => {
-    const sum_or_diff = function (sub, a, sign, b) {
+    const sum_or_diff = function(sub, a, sign, b) {
         return sign == '-' ? a - b : +a + +b;
     };
-    const mult_or_div = function (sub, a, sign, b) {
-        return sign == '*'|| sign =='•' ? a * b : a / b;
+    const mult_or_div = function(sub, a, sign, b) {
+        return sign == '*' || sign == '•' ? a * b : a / b;
     };
-    const power = function (sub, a, b) {
+    const power = function(sub, a, b) {
         return Math.pow(a, b);
     };
     const match_power = /(-?[\d\.]+)\s*\^\s*(-?[\d\.]+)/g;
     const match_mult_div = /(-?[\d\.]+)\s*([*•:\/])\s*(-?[\d\.]+)/g;
     const match_sum_diff = /(-?[\d\.]+)\s*([+-])\s*(-?[\d\.]+)/g;
 
-    const get_value = function (sub, exp) {
+    const get_value = function(sub, exp) {
         while (exp.indexOf('^') !== -1)
             exp = exp.replace(match_power, power);
         while (match_mult_div.test(exp))
             exp = exp.replace(match_mult_div, mult_or_div);
         while (match_sum_diff.test(exp))
             exp = exp.replace(match_sum_diff, sum_or_diff);
+        
         return exp;
     };
+
     while (str.indexOf('(') !== -1) // убираем скобки
         str = str.replace(/\(([^\(\)]*)\)/g, get_value);
 
