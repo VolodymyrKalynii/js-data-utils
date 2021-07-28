8aff1ad5d1e2a350519ec9e14674b6de5b244f6a
Volodymyr <v.kalynii@gmail.com>
2021-07-28 16:34:20 +0300

    upd math tests

---
+++ src/__tests__/math-utils.test.ts
@@ -0,0 +1,62 @@
+import {random} from 'lodash';
+import {calculateString, getNthDimensionNumber, round10} from '../math-utils';
+
+describe('round10', () => {
+    test('equal', () => {
+        expect(round10(55)).toBe(60);
+    });
+
+    for (let i = 1; i < 10; i++) {
+        const base = i * 10;
+        const dimension1 = random(1, 9);
+        const final = dimension1 >= 5 ? base + 10 : base;
+        const number = base + dimension1;
+
+        test(`equal by cycle, number ${number} to be ${final}`, () => {
+            expect(round10(number)).toBe(final);
+        });
+    }
+});
+
+describe('getNthDimensionNumber', () => {
+    for (let i = 1; i <= 5; i++) {
+        test(`equal for dimension ${i}`, () => {
+            const nthDimensionNumbers = [];
+
+            for (let j = 0; j < 20; j++) {
+                nthDimensionNumbers.push(getNthDimensionNumber(i));
+            }
+
+            expect(nthDimensionNumbers.every(number => number.toString().length === i)).toBe(true);
+        });
+    }
+});
+
+describe('calculateString', () => {
+    for (let i = 0; i < 50; i++) {
+        const fistNumber =  random(1, 9);
+        const secondNumber =  random(1, 9);
+
+        const multRes = fistNumber * secondNumber;
+        const divRes = fistNumber / secondNumber;
+        const addRes = fistNumber + secondNumber;
+        const subRes = fistNumber - secondNumber;
+        const powRes = Math.pow(fistNumber, secondNumber);
+
+        const cases = [
+            [multRes, '*'],
+            [multRes, '•'],
+            [divRes, '/'],
+            [divRes, ':'],
+            [addRes, '+'],
+            [subRes, '-'],
+            [powRes, '^'],
+        ];
+
+        cases.forEach(([result, action]) => {
+            test(`equal for ${action} ${result}`, () => {
+                expect(calculateString(`${fistNumber}${action}${secondNumber}`)).toBe(result);
+            });
+        });
+    }
+});
--- src/math-utils.ts
+++ src/math-utils.ts
@@ -3,40 +3,36 @@ import {random} from 'lodash';
 export const round10 = (value:number):number =>
     Math.round(value / 10) * 10;
 
-export const getRandNumber = ():number => parseInt(Math.random().toString().slice(2));
-
 /**
  * Returns a random value of a given bit rate
  */
-export const getNthDimensionNumber = (dimension:number) => {
+export const getNthDimensionNumber = (dimension:number):number => {
     const minFrame = Math.pow(10, dimension - 1);
     const maxFrame = Math.pow(10, dimension) - 1;
 
     return random(minFrame, maxFrame);
 };
 
-export const getRandFromFifthPercents = () => random(0, 1);
-
 /**
  * returns the result of calculations from the str
  * str: 1+1+1| 2-1-2 | 6/3*2| 2•2•2| 2:2•2| ...
- * tested for actions^ * / + -
+ * tested for actions: ^ * / + - •
  */
 export const calculateString = (str:string) => {
-    const sum_or_diff = function(sub, a, sign, b) {
+    const sum_or_diff = (sub, a, sign, b) => {
         return sign == '-' ? a - b : +a + +b;
     };
-    const mult_or_div = function(sub, a, sign, b) {
+    const mult_or_div = (sub, a, sign, b) => {
         return sign == '*' || sign == '•' ? a * b : a / b;
     };
-    const power = function(sub, a, b) {
+    const power = (sub, a, b) => {
         return Math.pow(a, b);
     };
     const match_power = /(-?[\d\.]+)\s*\^\s*(-?[\d\.]+)/g;
     const match_mult_div = /(-?[\d\.]+)\s*([*•:\/])\s*(-?[\d\.]+)/g;
     const match_sum_diff = /(-?[\d\.]+)\s*([+-])\s*(-?[\d\.]+)/g;
 
-    const get_value = function(sub, exp) {
+    const get_value = (sub, exp) => {
         while (exp.indexOf('^') !== -1)
             exp = exp.replace(match_power, power);
         while (match_mult_div.test(exp))
@@ -50,5 +46,7 @@ export const calculateString = (str:string) => {
     while (str.indexOf('(') !== -1) // убираем скобки
         str = str.replace(/\(([^\(\)]*)\)/g, get_value);
 
-    return parseInt(get_value('', str));
+    return +(get_value('', str));
 };
+
+console.log(calculateString('7/9'));
